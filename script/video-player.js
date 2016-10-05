ded8b253cb20e7bbe246c88420a402688c096c88
FaisalJulaidan <Julaidan.Faisal@gmail.com>
2016-10-06 00:18:34 +0100

    finish videos page and linked it with script to make it look like a video player

---
+++ script/video-player.js
@@ -0,0 +1,27 @@
+    function startVideo(clickedElement) {
+        var source = clickedElement.getAttribute("data-source");
+        var title = clickedElement.innerText;
+        var video = document.getElementById("active-video");
+
+        document.getElementById("video-title").innerText = title;
+        video.setAttribute("src", source);
+
+    }
+
+// not Working >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+    // function setDuration() {
+    //     var videosList = document.querySelectorAll(".hiddenVideo")
+    //     for (var i = 0; i < videosList.length; i++) {
+    //         var videoDuration = videosList[i].duration;
+    //         var durationCell = videosList[i].parentElement.nextElementSibling;
+    //
+    //         if (videoDuration >= 3600) {
+    //             durationCell.innerText = String(Math.round((videoDuration / 3600) * 100) / 100) + " hours"
+    //         } else if (videoDuration >= 60) {
+    //             durationCell.innerText = String(Math.round((videoDuration / 60) * 100) / 100) + " min";
+    //         } else {
+    //             durationCell.innerText = String(Math.round(videoDuration)) + " sec";
+    //         }
+    //     } //End for
+    // } // end setDuration()
+    // not Working >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
