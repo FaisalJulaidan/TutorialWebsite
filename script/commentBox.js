7324ed60daa380d2f4a1cfb82d7ef6d6a5c08c73
Kabira <sulemana@cardiff.ac.uk>
2016-10-19 18:35:06 +0100

    an aleart window pops up if the comment box left empty

--- script/commentBox.js
+++ script/commentBox.js
@@ -3,9 +3,11 @@ $("#addComment button").on("click", function(){
   var $textareaValue = $("#addComment textarea").val();
   var $comment = $("<p>" + $textareaValue + "</p> <br /> <hr />");
 
-  
+  if (/^\s*$/.test($textareaValue)){
+    alert("dont leave the comment box empty. Please!");
+  } else{
     $("#comments").append($comment);
-
+  }
 
 
 
