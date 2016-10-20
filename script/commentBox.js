5e427cbd6f50fa36a6458be2c6a0f379ce57aae6
Kabira <sulemana@cardiff.ac.uk>
2016-10-20 21:21:00 +0100

    validate the comment box and full name input field before it is been submited using Jquery

--- script/commentBox.js
+++ script/commentBox.js
@@ -1,7 +1,14 @@
 
 $("#addComment button").on("click", function(){
   var $textareaValue = $("#addComment textarea").val();
-  var $comment = $("<p>" + $textareaValue + "</p> <br /> <hr />");
+  var $firstNameValue = $("#name").val();
+
+  if (/^\s*$/.test($firstNameValue)){
+    $firstNameValue = "Anonymous";
+  }
+
+  // the $comment varible is under the above if statement to make sure that the variable firstNameValue take its up-to-date value.
+  var $comment = $("<h2>" + $firstNameValue + "<h2>" + "<p>" + $textareaValue + "</p> <br /> <hr />");
 
   if (/^\s*$/.test($textareaValue)){
     alert("dont leave the comment box empty. Please!");
