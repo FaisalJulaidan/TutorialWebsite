289f54ebf7ef5da1c270e76766e96b74b9146cdb
Faisal Julaidan <JulaidanFA@cardiff.ac.uk>
2016-10-24 23:11:48 +0100

    enhance the form vaildator script

--- script/validateForm.js
+++ script/validateForm.js
@@ -1,6 +1,10 @@
-var vaildName = false;
-var vaildEmail = false;
-var vaildTele = false;
+var validName = false;
+var validEmail = false;
+var validTele = false;
+var validSub = false;
+var validMsg = false;
+
+
 
 function validate($this, regExp, validMsg, invalidMsg) {
 
@@ -9,41 +13,68 @@ function validate($this, regExp, validMsg, invalidMsg) {
     var valid = '<span class="fa fa-check-circle"></span> ' + validMsg;
     var invalid = '<span class="fa fa-exclamation-circle "></span> ' + invalidMsg;
 
-    if ($value == '')
-        $this.attr("placeholder", "Don't leave it empty");
+
     if (regExp.test($value)) {
         $p.html(valid);
         $p.addClass("green").removeClass("red");
+         return true;
+
     } else {
+      if ($value == ''){
+          $this.attr("placeholder", "Don't leave it empty");
+      }
       $p.html(invalid);
-      $p.addClass("red").removeClass("green");    }
+      $p.addClass("red").removeClass("green");
+      return false;
+    }
 }
 
-
+//validating Full Name input
 $("#name").on("keyup change blur paste cut", function() {
-    validate($(this),
+  validName = validate($(this),
       /^([a-zA-Z]+[,.]?[ ]?|[a-zA-Z]+['-]?)+$/,
       "Valid Name",
       "Invaild Name, no Numbers, Double Spaces or Symbols !@#$%^*&()");
+
+      console.log(validName);
 });
 
-$("#email").on("blur paste cut", function() {
-    validate($(this),
+//validating Email input
+$("#email").on("blur keyup paste cut", function() {
+    validEmail = validate($(this),
       /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
       "Valid Email",
       "Invaild Email, should be something similar to <strong>tutorial_website@cardiff.co.uk</strong");
 });
 
+//validating Telephone input
 $("#tele").on("blur keyup paste cut", function() {
-    validate($(this),
+    validTele = validate($(this),
     /^\+?([0-9]{2,3}[-\s])?[0-9]{7,11}$/,
       "Valid Number",
-      "Invaild Number, no Letters");
+      "Invaild Number, no Letters, number of digits should be between 7-11");
 });
 
-$("#subject").on("blur", function() {
-    validate($(this),
+//validating Subject input
+$("#subject").on("blur keyup paste cut", function() {
+    validSub = validate($(this),
     /^[\w\s]+$/,
       "vaild Subject",
       "Invalid Subject");
+});
+
+$("#message").on("blur keyup paste cut", function() {
+    validMsg = validate($(this),
+    /^(?!\s*$).+/,
+      "vaild Message",
+      "Invalid Message, should't left empty!");
+});
+
+
+$("#submitBtn").on("click", function(){
+  if (validName && validEmail && validTele && validSub && validMsg){
+    alert("Message sent! Thank you for contacting us")
+  } else {
+    alert("Rejected, please fill all fields and use valid info. before you submit");
+  }
 });
